5a506c40b307e7a83673d533c7365450705cb26c
ahmdrozii <83721634+ahmdrozii@users.noreply.github.com>
2021-08-26 18:44:15 +0700

    Update __main__.py

--- JisooX/__main__.py
+++ JisooX/__main__.py
@@ -187,8 +187,7 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="➕ Add to groups",url="t.me/{}?startgroup=true".format(bot.username)),InlineKeyboardButton(text="Help & Commands",callback_data="help_back")]]
-    keyboard += [[InlineKeyboardButton(text="Owner of bot",url="https://t.me/eleaxzeno)]]
+    keyboard = [[InlineKeyboardButton(text="Help & Commands",callback_data="help_back"),InlineKeyboardButton(text="[► Add Me ◄]",url="t.me/{}?startgroup=true".format(bot.username))]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
